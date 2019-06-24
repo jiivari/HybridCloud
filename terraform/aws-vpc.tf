c102b8ef705989af8772500bcc9722936fb6c798
Jani Iivari <jani.iivari@siili.com>
2019-06-24 15:07:32 +0300

    Complate configs

--- terraform/aws-vpc.tf
+++ terraform/aws-vpc.tf
@@ -13,18 +13,59 @@ provider "aws" {
 }
 
 # Create a VPC
-resource "aws_vpc" "vpc2" {
-    cidr_block = "10.0.0.0/16"
+resource "aws_vpc" "vpc1" {
+    cidr_block = "192.168.0.0/16"
     tags = {
-        Name = "New VPC"
+        Name = "Default VPC"
     }
 }
 
-# Subnet
 resource "aws_subnet" "main" {
-  vpc_id            = "${aws_vpc.vpc2.id}"
-  cidr_block        = "${cidrsubnet(aws_vpc.vpc2.cidr_block, 4, 1)}"
+  vpc_id            = "${aws_vpc.vpc1.id}"
+  cidr_block        = "${cidrsubnet(aws_vpc.vpc1.cidr_block, 4, 1)}"
   tags = {
     Name = "main"
   }
+}
+
+# create customer gateway
+resource "aws_customer_gateway" "main" {
+  bgp_asn    = 65000
+  ip_address = "${azurerm_public_ip.gwpip.ip_address}"
+  type       = "ipsec.1"
+
+  tags = {
+    Name = "main-customer-gateway"
+  }
+  depends_on = ["azurerm_public_ip.gwpip"]
+}
+
+# create virtual private gateway
+resource "aws_vpn_gateway" "vpn_gw" {
+  vpc_id = "${aws_vpc.vpc1.id}"
+
+  tags = {
+    Name = "main"
+  }
+}
+
+# create vpn connection
+resource "aws_vpn_connection" "main" {
+  vpn_gateway_id      = "${aws_vpn_gateway.vpn_gw.id}"
+  customer_gateway_id = "${aws_customer_gateway.main.id}"
+  type                = "ipsec.1"
+  static_routes_only  = true
+}
+
+# Create vpn connection route to azure
+resource "aws_vpn_connection_route" "azure" {
+  destination_cidr_block = "${azurerm_subnet.subnet.address_prefix}"
+  vpn_connection_id      = "${aws_vpn_connection.main.id}"
+}
+
+# Create AWS to Azure Route
+resource "aws_route" "azureroute" {
+  route_table_id            = "${aws_vpc.vpc1.main_route_table_id}"
+  destination_cidr_block    = "${azurerm_subnet.subnet.address_prefix}"
+  vpc_peering_connection_id = "${aws_vpn_gateway.vpn_gw.id}"
 }
---
+++ terraform/azure-vnet.tf
@@ -0,0 +1,104 @@
+variable "azure_client_id" {}
+variable "azure_client_secret" {}
+variable "tenant_id" {}
+
+variable "location" {
+  default = "West Europe"
+}
+
+provider "azure" {
+    client_id = "${var.azure_client_id}"
+    client_secret = "${var.azure_client_secret}"
+    tenant_id = "${var.tenant_id}"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet1"
+  location            = "${var.location}"
+  resource_group_name = "hybridrg"
+  address_space       = ["172.16.0.0/16"]
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet1"
+  resource_group_name  = "hybridrg"
+  virtual_network_name = "${azurerm_virtual_network.vnet.name}"
+  address_prefix       = "172.16.1.0/24"
+}
+
+resource "azurerm_subnet" "GatewaySubnet" {
+  name                 = "GatewaySubnet"
+  resource_group_name  = "hybridrg"
+  virtual_network_name = "${azurerm_virtual_network.vnet.name}"
+  address_prefix       = "172.16.2.0/28"
+}
+
+resource "azurerm_public_ip" "gwpip" {
+  name                    = "vnetvgwpip1"
+  location                = "${var.location}"
+  resource_group_name     = "hybridrg"
+  allocation_method       = "Dynamic"
+  idle_timeout_in_minutes = 30
+}
+
+resource "azurerm_virtual_network_gateway" "vng" {
+  name                = "myvng1"
+  location            = "${var.location}"
+  resource_group_name = "hybridrg"
+
+  type     = "Vpn"
+  vpn_type = "RouteBased"
+
+  active_active = false
+  enable_bgp    = false
+  sku           = "VpnGw1"
+
+  ip_configuration {
+    name                          = "vnetGatewayConfig"
+    public_ip_address_id          = "${azurerm_public_ip.gwpip.id}"
+    private_ip_address_allocation = "Dynamic"
+    subnet_id                     = "${azurerm_subnet.GatewaySubnet.id}"
+  }
+
+}
+
+resource "azurerm_local_network_gateway" "lngw1" {
+  name                = "azlngw1"
+  resource_group_name = "hybridrg"
+  location            = "${var.location}"
+  gateway_address     = "${aws_vpn_connection.main.tunnel1_cgw_inside_address}"
+  address_space       = ["${aws_vpc.vpc1.cidr_block}"]
+}
+
+resource "azurerm_local_network_gateway" "lngw2" {
+  name                = "azlngw2"
+  resource_group_name = "hybridrg"
+  location            = "${var.location}"
+  gateway_address     = "${aws_vpn_connection.main.tunnel2_cgw_inside_address}"
+  address_space       = ["${aws_vpc.vpc1.cidr_block}"]
+}
+
+resource "azurerm_virtual_network_gateway_connection" "vngc1" {
+  name                = "vngc1"
+  location            = "${var.location}"
+  resource_group_name = "hybridrg"
+
+  type                       = "IPsec"
+  virtual_network_gateway_id = "${azurerm_virtual_network_gateway.vng.id}"
+  local_network_gateway_id   = "${azurerm_local_network_gateway.lngw1.id}"
+
+  shared_key = "${aws_vpn_connection.main.tunnel1_preshared_key}"
+}
+
+resource "azurerm_virtual_network_gateway_connection" "vngc2" {
+  name                = "vngc2"
+  location            = "${var.location}"
+  resource_group_name = "hybridrg"
+
+  type                       = "IPsec"
+  virtual_network_gateway_id = "${azurerm_virtual_network_gateway.vng.id}"
+  local_network_gateway_id   = "${azurerm_local_network_gateway.lngw2.id}"
+
+  shared_key = "${aws_vpn_connection.main.tunnel2_preshared_key}"
+}
+
